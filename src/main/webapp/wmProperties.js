8eecf7207fb87dcfa91a662472382468f1a8c9e2
root <root@61949b3d2f95>
2017-07-05 18:16:23 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_LoiKrEulFV",
+  "homePage" : "Main",
+  "name" : "Auto_LoiKrEulFV",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
